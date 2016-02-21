a85732dd9fb2be7daf54557d18c4d9863065ad87
Nik <nik@congral.com>
2016-02-21 10:45:09 -0500

    chore(sample): update version numbers

--- src/kendoui/button/k-button.js
+++ src/kendoui/button/k-button.js
@@ -1,14 +1,4 @@
 export class ButtonApi {
-    enabled = true;
-
-    disable() {
-      this.enabled = false;
-    }
-
-    enable() {
-      this.enabled = true;
-    }
-
     test() {
       alert('You clicked me');
     }
